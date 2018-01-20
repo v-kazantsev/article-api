e672cc70b649da4a04ae589ca87ec41179717936
v-kazantsev <vvs201@mail.ru>
2018-01-20 16:19:15 +1000

    Реализовал маршрут и метод для вывода статей по категории

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,8 +3,19 @@ class Api::ArticlesController < ApplicationController
   include ExceptionHandler
 
   # GET /api/articles/:id
-  def show
-    @article = Article.find(params[:id])
-    json_response(@article)
+  # def show
+  #   @article = Article.find(params[:id])
+  #   json_response(@article)
+  # end
+
+  def by_category
+    @articles = Article.where(category_id: Category.joins(:articles).where(name: params[:category]))
+    json_response(@articles)
   end
+
+  # def by_author
+  #   @articles = Article.where(author.last_name: params[:author])
+  #   json_response(@articles)
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
-    resources :articles, only: [:show]
+    resources :articles, only: :none
+    get 'articles/by_category'
+    get 'articles/by_author'
   end
 end
